2f96f284c911da47c4cc14614484bf0549e62aea
maccotter11 <122413834+maccotter11@users.noreply.github.com>
2024-06-30 10:30:30 -0400

    removed Emenies form Levels

--- galigo/levels.py
+++ galigo/levels.py
@@ -2,7 +2,7 @@ from entity import Entity
 from enemies import Enemies
 
 
-class Level(Entity, Enemies):
+class Level(Entity):
     def __init__(self, powerUP, enemiesNumUp):
         super().__init__()
         self.powerUP = powerUP
